b7b542ca216536f344799f7e2ea710473b48231e
Scott <sconover88@gmail.com>
2020-01-10 16:22:07 -0500

    Finished Assignment

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,6 +1,6 @@
 {
   $('button#play-pause').on('click', function() {
-    player.playPause();
+    helper.playPauseAndUpdate();
     $(this).attr('playState', player.playState);
   });
 
@@ -11,8 +11,7 @@
     const nextSongIndex = currentSongIndex + 1;
     if (nextSongIndex >= album.songs.length) { return; }
     const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
-
+    helper.playPauseAndUpdate(nextSong);
   });
 
   $('button#previous').on('click', function() {
@@ -22,7 +21,23 @@
     const nextSongIndex = currentSongIndex - 1;
     if (nextSongIndex >= album.songs.length) { return; }
     const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
-
+    helper.playPauseAndUpdate(nextSong);
   });
+
+  $('#time-control input').on('input', function (event) {
+    player.skipTo(event.target.value);
+    });
+
+    $('#volume-control input').on('input', function (event) {
+      player.setVolume(event.target.value);
+      });
+
+  setInterval( () => {
+    if (player.playState !== 'playing') { return; }
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control .current-time').text( currentTime );
+    $('#time-control input').val(percent);
+  }, 1000);
 }
